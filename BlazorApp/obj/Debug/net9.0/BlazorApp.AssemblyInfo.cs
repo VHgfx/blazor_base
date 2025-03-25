f81a095957f7597760fc0726ed09eb6c2e7f3485
VHgfx <hu-victor@hotmail.com>
2025-03-25 06:50:34 +0100

    Modification IncrementCount, display dynamique de la valeur du paramètre

--- BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad441221712f322dc4c595de47c2fb5121926e66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb57b4bc23c13474bdbafb1e10e20897a0b8876b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
